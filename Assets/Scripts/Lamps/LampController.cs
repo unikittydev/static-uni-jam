2f4380611dc995383f140bb15fe21cd0c2589bdf
unikittydev <777ruspacan777@gmail.com>
2023-06-27 00:18:54 +0300

    Update LampController.cs

--- Assets/Scripts/Lamps/LampController.cs
+++ Assets/Scripts/Lamps/LampController.cs
@@ -13,11 +13,6 @@ public class LampController : MonoBehaviour
         UpdateSprite();
     }
 
-    private void OnValidate()
-    {
-        UpdateSprite();
-    }
-    
     public void SwitchCondition()
     {
         condition = !condition;
